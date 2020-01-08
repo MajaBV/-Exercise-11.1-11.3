eb3142861b051dbcdae5f03ebc02a8b33a2e4caa
Maja Vukadin <maja.barisic5@gmail.com>
2020-01-08 18:47:15 +0100

    11.2: Add unsuccessful guesses into the dictionary

--- guessing_game.py
+++ guessing_game.py
@@ -5,6 +5,7 @@ import random
 player_name = input("What is your name? ")
 secret = random.randint(1, 30)
 broj_pokusaja = 0
+wrong_guesses = []
 
 with open("score.txt", "r") as score_file:
     best_score = json.loads(score_file.read())
@@ -12,15 +13,14 @@ with open("score.txt", "r") as score_file:
 
 
 for score_dict in best_score:
-    print("Dan/sat {0} Igrac {1} je imao/la {2} pokušaja. Skriveni broje je broj {3}".format(score_dict.get("datum"), score_dict.get("igrac"), score_dict.get("broj pokusaja"), score_dict.get("tajni broj")))
-
+    print("Dan/sat {0} Igrac {1} je imao/la {2} pokusaja. Skriveni broj je broj {3}. Pogresni pokusaji {4}".format(score_dict.get("datum"), score_dict.get("igrac"), str(score_dict.get("broj pokusaja")), score_dict.get("tajni broj"), score_dict.get("pogresni pokusaji")))
 
 while True:
     guess = int(input("Guess the secret number (between 1 and 30): "))
     broj_pokusaja = broj_pokusaja + 1
 
     if guess == secret:
-        best_score.append({"broj pokusaja": broj_pokusaja, "datum": str(datetime.datetime.now()), "igrac": player_name, "tajni broj": secret})
+        best_score.append({"broj pokusaja": broj_pokusaja, "datum": str(datetime.datetime.now()), "igrac": player_name, "tajni broj": secret, "pogresni pokusaji": wrong_guesses})
 
         with open("score.txt", "w") as score_file:
             score_file.write(json.dumps(best_score))
@@ -40,3 +40,4 @@ while True:
     else:
         print("Sorry, your guess is not correct... The secret number is not " + str(guess))
 
+    wrong_guesses.append(guess)
